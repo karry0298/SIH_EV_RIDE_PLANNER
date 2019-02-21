97302b4eaf547cd200b792f313fb999484e85399
kartick <kartick.hariharan@gmail.com>
2019-02-21 12:17:28 +0530

    no navigation

---
+++ React-native-app/EvRider/app/components/NearMeList/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        abc:1
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> Make Maps here with the nearbay lists </Text>
+      </View>
+    );
+  }
+}
---
+++ React-native-app/EvRider/app/components/Sidebar/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { AppRegistry, Image, StatusBar,Text } from "react-native";
+import { Button,Container,List,ListItem,Content,Icon,Thumbnail} from "native-base";
+
+//const routes = ["Cam", "Ram"];
+
+const routes = [{title:"cam" , icon:"map-marked-alt"},{title:"ram" , icon:"user"}];
+
+export default class SideBar extends React.Component {
+  render() {
+    
+    return (    
+      
+      <Container>
+        <Content>  
+          
+          <Text blurRadius={1} style={{color:'white' , left:19 ,right:2 , fontSize:17 , top:19, elevation:3}}>Welcome To Night Raid's Profile</Text>
+        
+          <List
+            dataArray={routes}
+            contentContainerStyle={{ marginTop: 35 }}
+            renderRow={data => {
+              return (
+                <ListItem button onPress={() => this.props.navigation.navigate(data.title)}>
+                  <Text blurRadius={1} style={{color:'#ffffff' , fontSize:25, paddingLeft:20 ,paddingRight:5, elevation:3,fontFamily:"courbd"}}>{data.title}</Text>
+                </ListItem>
+              );
+            }}
+          />
+        </Content>
+      </Container>
+
+    );
+  }
+}
