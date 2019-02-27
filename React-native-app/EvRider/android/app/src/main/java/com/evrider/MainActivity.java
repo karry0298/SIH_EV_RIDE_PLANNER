9a5691ae398e72c8a05dcc23e219587a10131573
Ashley Lobo <ashleyl@rediffmail.com>
2019-02-27 09:57:08 +0530

    Changes made in Main activiity

--- React-native-app/EvRider/android/app/src/main/java/com/evrider/MainActivity.java
+++ React-native-app/EvRider/android/app/src/main/java/com/evrider/MainActivity.java
@@ -1,6 +1,8 @@
 package com.evrider;
 
 import com.facebook.react.ReactActivity;
+import android.content.Intent; // <-- include if not already there
+import com.tkporter.sendsms.SendSMSPackage;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +14,11 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "EvRider";
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    //probably some other stuff here
+    SendSMSPackage.getInstance().onActivityResult(requestCode, resultCode, data);
+}
 }
