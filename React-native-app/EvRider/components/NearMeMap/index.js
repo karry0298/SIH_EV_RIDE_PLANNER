86245bd0fdba134dcec9ef7dfc5f18a9ddcfeb40
Gauri Jare <jaregauri1998@gmail.com>
2019-03-03 09:26:40 +0530

    edwin changes

--- React-native-app/EvRider/components/NearMeMap/index.js
+++ React-native-app/EvRider/components/NearMeMap/index.js
@@ -327,14 +327,20 @@ else{
       <View style={styles.container}>
     
         <View style={{flexDirection:"row"}}>
-                <Button style={{marginLeft:1,backgroundColor:"red",paddingLeft:11,paddingRight:15}} >
-                    <Text style={{fontSize:21 , paddingLeft:25 ,color:"white"}} > {"charge:20%"} </Text>
+              <Button style={{backgroundColor:"white",paddingLeft:5,paddingRight:5}}
+                      onPress={() => {this.props.navigation.navigate('nearmeMap')}}>
+                <Text style={{fontSize:21}}>Map </Text>
+                <FontAwesome5 name={"map-marked-alt"} brand style={{paddingLeft:5 , fontSize: 20, color:'black'}} />
+              </Button>
+
+                <Button style={{marginLeft:1,backgroundColor:"white",paddingLeft:5,paddingRight:5}} >
+                    <Text style={{fontSize:21 , paddingLeft:25 ,color:"black"}} > {"charge:20%"} </Text>
                     <FontAwesome5 name={"battery-three-quarters"} brand style={{transform: [{ rotate: '270deg'}],marginBottom:23 ,fontSize: 20, color:"white" , paddingRight:25}} />        
                 </Button>
 
-                <Button style={{marginLeft:1,backgroundColor:"red",paddingLeft:23,paddingRight:27}} 
+                <Button style={{marginLeft:1,backgroundColor:"white",paddingLeft:23,paddingRight:27}}
                         onPress={() => {this.props.navigation.navigate('nearmelist',{abc:this.state.myStateFinale})}}>
-                    <Text style={{fontSize:21,paddingLeft:30,color:"white"}} > List </Text>
+                    <Text style={{fontSize:21,paddingLeft:30,color:"black"}} > List </Text>
                     <FontAwesome5 name={"list-ul"} brand style={{paddingLeft:5 ,paddingRight:50 , fontSize: 20, color:"white"}} />        
                 </Button>
         </View>
